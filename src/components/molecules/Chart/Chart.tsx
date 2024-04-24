839c50eff5eb216c6dcaab5994995a901dc3aa0e
isavega <igvega02@gmail.com>
2024-04-24 16:41:31 -0400

    feat: add data to chart

--- src/components/molecules/Chart/Chart.tsx
+++ src/components/molecules/Chart/Chart.tsx
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
+import { getTimestamps } from "../../../utils/index.tsx";
+import useTrades from "../../../hooks/useTrades.tsx";
+
+const crypto = "btc";
+const currency = "clp";
+const amount = 1000000;
+const frequency = "daily";
+const startDate = "2021-01-01";
+const endDate = "2021-01-13";
 
 const Chart: React.FC = () => {
+  const getDates = useMemo(
+    () => getTimestamps(startDate, endDate, frequency),
+    []
+  );
+
+  console.log(getDates);
+
+  const { trades } = useTrades(`BTC-CLP`, getDates);
+
+  const calculatePrices = useCallback(() => {
+    const prices = trades.map((trade) => {
+      const total = trade.trades.entries.reduce(
+        (acc: number, [quantity, price]: [string, string]) => {
+          return acc + parseFloat(quantity) * parseFloat(price);
+        },
+        0
+      );
+
+      const totalQuantity = trade.trades.entries.reduce(
+        (acc: number, [quantity]: [string, string]) => {
+          return acc + parseFloat(quantity);
+        },
+        0
+      );
+
+      return total / totalQuantity;
+    });
+
+    return prices;
+  }, [trades]);
+
+  const calculateTimestamps = useCallback(() => {
+    const timestamps = trades.map((trade) => trade.trades.timestamp);
+    return timestamps;
+  }, [trades]);
+
+  const prices = calculatePrices();
+  const timestamps = calculateTimestamps();
+
   return (
     <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+      xAxis={[{ data: timestamps }]}
       series={[
         {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
+          data: prices,
           area: true,
         },
       ]}
