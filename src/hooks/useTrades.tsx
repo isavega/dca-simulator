c3b0ae9b39adf4b67ac556ecd1a816c8e9ed2b54
isavega <igvega02@gmail.com>
2024-04-25 17:27:13 -0400

    fix: prices in use trade hook

--- src/hooks/useTrades.tsx
+++ src/hooks/useTrades.tsx
@@ -1,25 +1,75 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { getTrades } from "../api/buda.tsx";
 
 const useTrades = (marketId: string, timestampArray: Array<number>) => {
-  const [trades, setTrades] = React.useState<any>([]);
+  const [averagePrices, setAveragePrices] = useState<Array<number>>([]);
 
   useEffect(() => {
-    const fetchTrades = async () => {
+    const fetchTradesAndCalculateAverage = async () => {
       try {
-        const trades = await Promise.all(
+        const tradesData = await Promise.all(
           timestampArray.map((timestamp) => getTrades(marketId, timestamp))
         );
-        setTrades(trades);
+
+        const calculatedPrices = tradesData.map((trade) => {
+          const entries = trade.trades.entries;
+          let totalAmount = 0;
+          let totalPrice = 0;
+
+          entries.forEach((entry) => {
+            const amount = parseFloat(entry[1]);
+            const price = parseFloat(entry[2]);
+            totalAmount += amount;
+            totalPrice += amount * price;
+          });
+
+          const averagePrice = totalAmount > 0 ? totalPrice / totalAmount : 0;
+          return Math.round(averagePrice * 100) / 100; // Truncar a 2 decimales
+        });
+
+        setAveragePrices(calculatedPrices);
       } catch (error) {
-        console.error("Failed to fetch trades:", error);
+        console.error(
+          "Failed to fetch trades or calculate average prices:",
+          error
+        );
       }
     };
 
-    fetchTrades();
+    fetchTradesAndCalculateAverage();
   }, [marketId, timestampArray]);
 
-  return { trades };
+  return { averagePrices };
 };
 
 export default useTrades;
+
+// const calculatePrices = useCallback(() => {
+//   const prices = trades.map((trade) => {
+//     const total = trade.trades.entries.reduce(
+//       (acc: number, [quantity, price]: [string, string]) => {
+//         return acc + parseFloat(quantity) * parseFloat(price);
+//       },
+//       0
+//     );
+
+//     const totalQuantity = trade.trades.entries.reduce(
+//       (acc: number, [quantity]: [string, string]) => {
+//         return acc + parseFloat(quantity);
+//       },
+//       0
+//     );
+
+//     return total / totalQuantity;
+//   });
+
+//   return prices;
+// }, [trades]);
+
+// const calculateTimestamps = useCallback(() => {
+//   const timestamps = trades.map((trade) => trade.trades.timestamp);
+//   return timestamps;
+// }, [trades]);
+
+// const prices = calculatePrices();
+// const timestamps = calculateTimestamps();
